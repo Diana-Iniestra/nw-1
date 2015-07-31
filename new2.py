cf06b8b406e7d64a843f7bdd2d7d27d694ab774a
Vicente <vincent_97@live.com.mx>
2015-07-31 15:20:58 -0500

    pruebas

--- new2.py
+++ new2.py
@@ -1,16 +1,24 @@
 import sys, random
 import os
-from threading import Timer
-### We must set OS env before the pygame imports..
 import pygame
 from pygame.locals import *
 from pygame.color import *
 
-display_size = (600,600)
-
 import pymunk as pm
 from pymunk import Vec2d
-    
+
+display_size = (600,600)
+##############################################################################
+class Circle(object):
+    def __init__(self,screen, colour, origin, radius, width=0):
+        self.screen = screen
+        self.colour = colour
+        self.origin = origin
+        self.radius = radius
+        self.width = width
+    def draw(self,image, colour, origin, radius, width):
+        pygame.draw.circle(self.image,self.colour,sefl.origin,int(self.radius))
+
 def drawcircle(image, colour, origin, radius, width=0):
     if width == 0:
         pygame.draw.circle(image,colour,origin,int(radius))
@@ -25,19 +33,17 @@ def drawcircle(image, colour, origin, radius, width=0):
         image.blit(circle, [origin[0] - (circle.get_width()/2), origin[1] - (circle.get_height()/2)])    
 
         print THECOLORS
-
+##############################################################################
 def reset_bodies(space):
     for body in space.bodies:
         body.position = Vec2d(body.start_position)
         body.reset_forces()
         body.velocity = 0,0
         body.angular_velocity = 0
-    color = THECOLORS["red"]
     for shape in space.shapes:
-        shape.color = color
-    
+        shape.color = THECOLORS["red"]
+   ############################################################################## 
 def main():
-    
     pygame.init()
     screen = pygame.display.set_mode(display_size) 
     width, height = screen.get_size()
@@ -51,14 +57,16 @@ def main():
     clock = pygame.time.Clock()
     running = True
     font = pygame.font.Font(None, 16)
-    
+##############################################################################
+
     ### Physics stuff
     space = pm.Space(iterations = 1)
     space.gravity = (0.0, -1900.0)
     space.damping = 0.8 # to prevent it from blowing up.
     static_body = pm.Body()
     mouse_body = pm.Body()
-   
+##############################################################################
+
     bodies = []
     for x in range(-100,150,50):
         x += width / 2
@@ -79,28 +87,28 @@ def main():
         
     reset_bodies(space)
     selected = None
-        
+##############################################################################
+
     while running:
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
-            elif event.type == KEYDOWN and event.key == K_p:
-                pygame.image.save(screen, "newtons_cradle.png")
-            
             if event.type == pygame.USEREVENT+1:
                 r = random.randint(1,4)
                 for body in bodies[0:r]:
                     body.apply_impulse((-6000,0))
             if event.type == pygame.USEREVENT+2:
                 reset_bodies(space)
-                
+##############################################################################
+
             elif event.type == KEYDOWN and event.key == K_r:
                 reset_bodies(space)
             elif event.type == KEYDOWN and event.key == K_f:
                 r = random.randint(1,4)
                 for body in bodies[0:r]:
                     body.apply_impulse((-6000,0))
-                
+    ##############################################################################
+
             elif event.type == MOUSEBUTTONDOWN:
                 if selected != None:
                     space.remove(selected)
@@ -111,12 +119,12 @@ def main():
                     ds = pm.DampedSpring(mouse_body, shape.body, (0,0), (0,0), rest_length, 1000, 10)
                     space.add(ds)
                     selected = ds
-            
+    ##############################################################################
+
             elif event.type == MOUSEBUTTONUP:
                 if selected != None:
                     space.remove(selected)
                     selected = None
-                    
             elif event.type == KEYDOWN:
                 running = False
             elif event.type == MOUSEBUTTONDOWN:
@@ -125,10 +133,10 @@ def main():
         mpos = pygame.mouse.get_pos()
         p = from_pygame( Vec2d(mpos) )
         mouse_body.position = p
-        
+##############################################################################
+
         ### Clear screen
         screen.fill(THECOLORS["black"])
-        
         ### Draw stuff
         for c in space.constraints:
             pv1 = c.a.position + c.anchr1
@@ -136,22 +144,20 @@ def main():
             p1 = to_pygame(pv1)
             p2 = to_pygame(pv2)
             pygame.draw.aalines(screen, THECOLORS["yellow"], False, [p1,p2])
-            
+##############################################################################
+
         for ball in space.shapes:
             p = to_pygame(ball.body.position)
             drawcircle(screen, ball.color, p, int(ball.radius), 0)
             #pygame.draw.circle(screen, ball.color, p, int(ball.radius), 0)
-        tim = 0
-        #velocity
-        #slow_down_velocity(tim)
-        ### Update physics
+##############################################################################
+
         fps = 50
         iterations = 25
         dt = 1.0/float(fps)/float(iterations)
         for x in range(iterations): # 10 iterations to get a more stable simulation
             space.step(dt)
-        
-        ### Flip screen
+##############################################################################
 
         screen.blit(font.render("fps: " + str(clock.get_fps()), 1, THECOLORS["white"]), (0,0))
         pygame.display.flip()
